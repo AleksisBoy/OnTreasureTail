7079389098789616842cd8ff2616d2e2b3efd322
AleksisBoy <aleboj9@gmail.com>
2024-06-03 11:56:14 +0200

    Imported trees

--- Assets/External/AleM_BezierCurve/Scripts/PlaceRandomlyOnCurve.cs
+++ Assets/External/AleM_BezierCurve/Scripts/PlaceRandomlyOnCurve.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class PlaceRandomlyOnCurve : MonoBehaviour
 {
     [SerializeField] private Transform[] objects = null;
-    [SerializeField] private bool randomRotationY = false;
+    [SerializeField] private bool randomRotation = false;
+    [SerializeField] private Vector3 rotationRange = Vector3.zero;
+    [SerializeField] private Vector3 scaleRange1 = Vector3.zero;
+    [SerializeField] private Vector3 scaleRange2 = Vector3.zero;
 
     private BezierCurve curve = null;
     private void OnEnable()
@@ -19,9 +22,9 @@ public class PlaceRandomlyOnCurve : MonoBehaviour
         {
             if (TryGetComponent<BezierCurve>(out BezierCurve newCurve)) curve = newCurve; 
         }
-        if (randomRotationY)
+        if (randomRotation)
         {
-            PlaceRandomlyWithRotationY();
+            PlaceRandomlyWithRotation();
             return;
         }
         foreach (Transform t in objects)
@@ -29,12 +32,13 @@ public class PlaceRandomlyOnCurve : MonoBehaviour
             t.position = curve.GetRandomPositionOnCurve() + transform.position;
         }
     }
-    public void PlaceRandomlyWithRotationY()
+    public void PlaceRandomlyWithRotation()
     {
         foreach(Transform t in objects)
         {
             t.position = curve.GetRandomPositionOnCurve() + transform.position;
-            t.rotation = Quaternion.Euler(0f, Random.Range(-180f, 180f), 0f);
+            t.rotation = Quaternion.Euler(Random.Range(-rotationRange.x, rotationRange.x), Random.Range(-rotationRange.y, rotationRange.y), Random.Range(-rotationRange.z, rotationRange.z));
+            t.localScale = new Vector3(Random.Range(scaleRange1.x, scaleRange2.x), Random.Range(scaleRange1.y, scaleRange2.y), Random.Range(scaleRange1.z, scaleRange2.z));
         }
     }
 }
